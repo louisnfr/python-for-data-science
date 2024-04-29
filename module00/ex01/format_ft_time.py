ffddc200d700b24e38fb47898a573e3a88042e04
Louis RAFFIN <lraffin@bess-f4r3s6.clusters.42paris.fr>
2024-04-30 00:30:17 +0200

    m00e01 done

--- module00/ex01/format_ft_time.py
+++ module00/ex01/format_ft_time.py
@@ -1,5 +1,9 @@
 import datetime
 
-utc = datetime.date(1970, 1, 1)
+now = datetime.datetime.now()
+utc = datetime.datetime(1970, 1, 1)
 
-print(utc)
+secondsElapsed = (now - utc).total_seconds()
+
+print("Seconds since January 1, 1970:", f"{secondsElapsed:,.4f}", "or", f"{secondsElapsed:.3}", "in scientific notation")
+print(now.strftime("%b %d %Y"))
