3e215374410ee3388467a3dabd739d02e549a36c
louisnfr <louis.raffin@gmail.com>
2024-05-15 18:32:02 +0200

    ex01 done

--- module01/ex00/give_bmi.py
+++ module01/ex00/give_bmi.py
@@ -1,8 +1,13 @@
 def give_bmi(
     height: list[int | float], weight: list[int | float]
 ) -> list[int | float]:
+    """gives the BMI of a person based on their height and weight."""
     return [weight[i] / height[i] ** 2 for i in range(len(height))]
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    takes a list of BMI values and a limit, and returns a list of booleans
+    indicating whether each BMI value is above the limit.
+    """
     return [val > limit for val in bmi]
---
+++ module01/ex01/array2D.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    """slice_me returns a slice of a 2D array"""
+    try:
+        arr = np.array(family)
+        print("my shape is:", arr.shape)
+        print("my new shape is:", arr[start:end].shape)
+    except ValueError as e:
+        print(e)
+    return arr[start:end]
