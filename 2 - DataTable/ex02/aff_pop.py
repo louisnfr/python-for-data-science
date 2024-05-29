db74b9bfac05e8926dfff3b15aae201dd3503e78
louisnfr <louis.raffin@gmail.com>
2024-05-30 00:06:28 +0200

    ex02 done

--- 2 - DataTable/ex02/aff_pop.py
+++ 2 - DataTable/ex02/aff_pop.py
@@ -2,12 +2,19 @@ import matplotlib.pyplot as plt
 from load_csv import load
 
 
+def y_formatter(x: float, pos: float) -> str:
+    """format y-axis"""
+    if x >= 1_000_000:
+        return f"{x/1_000_000:.0f}M"
+    return f"{x/1_000}k"
+
+
 def convert_to_float(number: str) -> float:
     """convert number string to float"""
     if number.endswith("k"):
-        return float(number[:-1]) * 1000
+        return float(number[:-1]) * 1_000
     if number.endswith("M"):
-        return float(number[:-1]) * 1000000
+        return float(number[:-1]) * 1_000_000
     return float(number)
 
 
@@ -16,21 +23,26 @@ def main():
     data = load("population_total.csv")
     if data.empty:
         return
+
     france = data[data["country"] == "France"]
     zambia = data[data["country"] == "Zambia"]
-    years = data.columns[1:]
+    years = data.columns[1:-50]
 
-    zambiaPop = [convert_to_float(val) for val in zambia.values[0][1:]]
-    francePop = [convert_to_float(val) for val in france.values[0][1:]]
+    zambiaPop = [convert_to_float(val) for val in zambia.values[0][1:-50]]
+    francePop = [convert_to_float(val) for val in france.values[0][1:-50]]
 
-    plt.title("Population Projections")
     plt.plot(years, francePop, label="France")
     plt.plot(years, zambiaPop, label="Zambia")
-    plt.legend()
+
+    plt.title("Population Projections")
+    plt.xlabel("Year")
+    plt.ylabel("Population")
+    plt.legend(loc="lower right")
+
     plt.xticks(years[::50])
-    maxPop = max(max(francePop), max(zambiaPop))
-    yTicks = [i * 1e7 for i in range(int(maxPop / 1e7) + 1)]
-    plt.yticks(yTicks, ["{:,.0f}M".format(pop / 1e6) for pop in yTicks])
+    plt.yticks([20_000_000, 40_000_000, 60_000_000])
+    plt.gca().yaxis.set_major_formatter(y_formatter)
+
     plt.show()
 
 
