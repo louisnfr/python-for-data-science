ce831ebfcefd84942ee5a5272be25d335bd5cd8f
louisnfr <louis.raffin@gmail.com>
2024-05-31 22:56:25 +0200

    e

---
+++ 3_oop/ex04/ft_calculator.py
@@ -0,0 +1,16 @@
+class calculator:
+    def __init__(self, vector: list[float]):
+        """init"""
+        self.vector = vector
+
+    # decorator
+    def dotproduct(v1: list[float], v2: list[float]) -> None:
+        pass
+
+    # decorator
+    def add_vec(v1: list[float], v2: list[float]) -> None:
+        pass
+
+    # decorator
+    def sous_vec(v1: list[float], v2: list[float]) -> None:
+        pass
