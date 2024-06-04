cba62d15dc0d1a0458d65ef142673597fdef73bc
louisnfr <louis.raffin@gmail.com>
2024-06-04 17:39:17 +0200

    quartil

--- 4_dod/ex00/statistics.py
+++ 4_dod/ex00/statistics.py
@@ -1,4 +1,6 @@
 def median(args: list[int]) -> float:
+    """returns median of list"""
+    print(f"median of {args}")
     sort = sorted(args)
     middle = len(sort) // 2
     if len(sort) % 2 == 0:
@@ -8,12 +10,27 @@ def median(args: list[int]) -> float:
 
 
 def mean(args: list[int]) -> float:
+    """returns mean of list"""
     return sum(args) / len(args)
 
 
+def quartile(args: list[int]):
+    """returns q1 and q3 of list"""
+    sort = sorted(args)
+    middle = len(sort) // 2
+
+    q1 = median(sort[:middle])
+    q3 = median(sort[-middle:])
+    print(f"Q1 {q1}\nQ3 {q3}")
+    return [q1, q3]
+    print(f"q1: {q1}\nq3: {q3}")
+
+
 def ft_statistics(*args: int, **kwargs: str) -> None:
-    array = list(args)
+    """main function"""
+    # array = list(args)
     # for arg in args:
     #     print(arg)
     # for key, value in kwargs.items():
     #     print(f"{key}={value}")
+    pass
--- 4_dod/ex00/tester.py
+++ 4_dod/ex00/tester.py
@@ -1,8 +1,12 @@
-from statistics import mean, median  # type: ignore
+from statistics import quartile  # type: ignore
 
-print(median([1]))
-print(median([9, 10, 12, 13, 13, 13, 15, 15, 16, 16, 18, 22, 23, 24, 24, 25]))
-print(median([10, 2, 38, 23, 38, 23, 21]))
+quartile([1, 3, 4, 5])
+print()
+quartile([1, 11, 1, 360])
+
+# print(quartile([1, 2, 3]))
+# print(quartile([9, 10, 12, 13, 13, 13, 15, 15, 16, 16, 18, 22, 23, 24, 24, 25]))
+# print(quartile([10, 2, 38, 23, 38, 23, 21]))
 
 # ft_statistics(1, 42, 360, 11, 64, toto="mean", tutu="median", tata="quartile")
 # print("-----")
